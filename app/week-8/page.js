b0c6275e2f390e00fd85904c678e3f5ffb7d78c3
Brady Maki <78286012+bradym05@users.noreply.github.com>
2026-03-10 13:56:09 -0600

    week 8 navigation
    added buttons

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -13,25 +13,32 @@ export default function Page() {
         await firebaseSignOut();
     };
 
-    return (
+    return user ? (
         <div className="flex flex-col items-center justify-center">
+            <h1 className="shadow-xs text-white font-bold m-4">
+                Welcome, {user.displayName} ({user.email})
+            </h1>
+            <a href="./week-8/shopping-list" className="cursor:pointer hover:bg-white text-gray-600 hover:text-gray-800 p-3 m-4 bg-gray-300 rounded-2xl shadow-xs text-black font-semibold">
+                Shopping List
+            </a>
+            <button onClick={handleSignOut} className="hover:bg-white text-gray-600 hover:text-gray-800 p-3 m-4 bg-gray-300 rounded-2xl shadow-xs text-black font-semibold">
+                Sign out
+            </button>
+        </div>
 
-            {user ? (
-                <h1 className="shadow-xs text-white font-bold m-4">
-                    Welcome, {user.displayName} ({user.email})
-                </h1>
-            ) : (
-                <h1 className="shadow-xs text-white font-bold m-4">
-                    Not signed in
-                </h1>
-            )}
-
-            <button onClick={handleSignIn} className="hover:bg-white text-gray-600 hover:text-gray-800 p-3 m-4 bg-gray-300 rounded-2xl shadow-xs text-black font-semibold">
+    ) : (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="shadow-xs text-white font-bold m-4">
+                Not signed in
+            </h1>
+            <button onClick={handleSignIn} className="cursor:pointer hover:bg-white text-gray-600 hover:text-gray-800 p-3 m-4 bg-gray-300 rounded-2xl shadow-xs text-black font-semibold">
                 Sign in with GitHub
             </button>
             <button onClick={handleSignOut} className="hover:bg-white text-gray-600 hover:text-gray-800 p-3 m-4 bg-gray-300 rounded-2xl shadow-xs text-black font-semibold">
                 Sign out
             </button>
         </div>
-    );
+    )
+
+
 }
